27ac583f707a2e9b00aa94e22b37aba8fd90525c
kristiansja <firefall1@hotmail.no>
2020-09-28 11:46:28 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace RunAll_2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(22, 24);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(400, 324);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::RunAll.Properties.Resources.wallp;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(453, 373);
+            this.Controls.Add(this.listBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(475, 429);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(475, 429);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "RunAll";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics;
+namespace RunAll_2._0
+{
+    public partial class Form1 : Form
+    {
+
+        List<Process> ProcessList = new List<Process>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            listBox1.BackColor = Color.FromArgb(26, 20, 16);
+            listBox1.ForeColor = Color.White;
+            DirectoryInfo directoryInfo = new DirectoryInfo(Assembly.GetAssembly(typeof(Program)).Location.Replace(System.AppDomain.CurrentDomain.FriendlyName, "") + @"shortcuts");
+            List<string> list = new List<string>();
+
+            foreach (FileInfo fileInfo in directoryInfo.GetFiles("*.lnk"))
+            {
+                System.Diagnostics.Process process = new Process();
+                process.StartInfo.FileName = fileInfo.FullName;
+                process.Start();
+                ProcessList.Add(process);
+                listBox1.Items.Add(fileInfo.Name);
+            }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            foreach (var prop in ProcessList)
+            {
+                try {
+                    prop.Kill();
+                } catch(Exception k) {
+                    continue;
+                }
+            }
+
+            this.Name = (Process.GetProcessesByName("AutoHotkey").Length.ToString());
+
+            foreach (var proP in Process.GetProcessesByName("AutoHotkey")) {
+                proP.Kill();
+            }
+        }
+    }
+}
